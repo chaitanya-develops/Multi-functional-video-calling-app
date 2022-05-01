36d42ef823adf4a02d97c7872ea4db5244e60404
chaitanya-develops <krishnachaitanya.reddy@iiitb.ac.in>
2022-05-02 00:58:33 +0530

    Create login and register components and add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Switch, Route, Redirect} from 'react-router-dom'
+import Login from './Login/Login';
+import Register from './Register/Register';
 
 function App() {
-  return (
-    <div className="App">
-          </div>
+  return ( <>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/register">
+          <Register />
+        </Route>
+        <Route path= "/">
+          <Redirect to="/login" />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+    </>
   );
 }
 
