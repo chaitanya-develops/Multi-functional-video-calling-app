ed1bdfc4532241360d566c8b37d5ae311d5c66d5
chaitanya-develops <krishnachaitanya.reddy@iiitb.ac.in>
2022-05-08 14:40:25 +0530

    Add joi validation for authentication

--- backend/routes/authenticationRoutes.js
+++ backend/routes/authenticationRoutes.js
@@ -1,8 +1,27 @@
 var express = require("express");
 var router = express.Router();
 var postAuthentication = require("../controllers/postAuthentication");
+const Joi = require("joi");
+const validator = require("express-joi-validation").createValidator({});
+const auth = require("../middleware/auth");
 
-router.post("/login",postAuthentication.controllers.loginPost);
-router.post("/register",postAuthentication.controllers.registerPost);
+const registerSchema = Joi.object({
+    username: Joi.string().min(3).max(12).required(),
+    password: Joi.string().min(4).max(12).required(),
+    email: Joi.string().email().required(),
+});
+  
+const loginSchema = Joi.object({
+    password: Joi.string().min(4).max(12).required(),
+    email: Joi.string().email().required(),
+});
+
+router.post("/login",validator.body(loginSchema),postAuthentication.controllers.loginPost);
+router.post("/register",validator.body(registerSchema),postAuthentication.controllers.registerPost);
+
+// test route to verify if our middleware is working
+router.get("/test", auth, (req, res) => {
+    res.send("request passed");
+  });
 
 module.exports = router;
