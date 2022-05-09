8da0f037b3fb5c661b027d131221696cd6743f4c
akhila kondapalli <akhila.kondapalli@iiitb.ac.in>
2022-05-09 14:32:06 +0530

    Update messenger UI

--- frontend/src/Dashboard/Messenger/Messenger.js
+++ frontend/src/Dashboard/Messenger/Messenger.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { styled } from "@mui/system";
 import AppBar from "../AppBar/AppBar";
+import { connect } from "react-redux";
+import WelcomeMessage from "./WelcomeMessage";
+import MessengerContent from "./MessengerContent";
 
 const MainContainer = styled("div")({
   
@@ -13,10 +16,18 @@ const MainContainer = styled("div")({
   display: "flex",
 });
 
-const Messenger = () => {
-  return <MainContainer>
+const Messenger = (chosenChatDetails) => {
+  return (
+  <MainContainer>
     <AppBar />
-  </MainContainer>;
+    {!chosenChatDetails ? (<WelcomeMessage />) : (<MessengerContent chosenChatDetails={chosenChatDetails} />)} 
+    
+  </MainContainer>);
 };
 
-export default Messenger;
+const mapStoreStateToProps = ({chat}) => {
+  return {
+    ...chat,
+  };
+};
+export default connect(mapStoreStateToProps) (Messenger);
